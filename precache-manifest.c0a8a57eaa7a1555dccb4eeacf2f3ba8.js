f25babeab1c000c8c6b443a655453e5535db46ad
yotoz <thdrudwls7@gmail.com>
2020-02-13 02:38:34 +0900

    Updates

---
+++ precache-manifest.c0a8a57eaa7a1555dccb4eeacf2f3ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8126630820998915ce8738efc97012",
+    "url": "/yotoz/ImageCropper/index.html"
+  },
+  {
+    "revision": "af7a3868cb5eedee5684",
+    "url": "/yotoz/ImageCropper/static/css/2.ee8f07d3.chunk.css"
+  },
+  {
+    "revision": "7e3790c4d7dcefe3bb72",
+    "url": "/yotoz/ImageCropper/static/css/main.100c8ad1.chunk.css"
+  },
+  {
+    "revision": "af7a3868cb5eedee5684",
+    "url": "/yotoz/ImageCropper/static/js/2.bef3363c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/yotoz/ImageCropper/static/js/2.bef3363c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3790c4d7dcefe3bb72",
+    "url": "/yotoz/ImageCropper/static/js/main.c1f29282.chunk.js"
+  },
+  {
+    "revision": "62af80ed88895af19339",
+    "url": "/yotoz/ImageCropper/static/js/runtime-main.3af437ac.js"
+  }
+]);
